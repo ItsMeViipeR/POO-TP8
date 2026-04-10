c21c5f971bfde1928e9b968ee59f85501e9227b4
ItsMeViipeR <skkyz3r.dev@gmail.com>
2026-04-10 14:48:00 +0200

    chore: Uses the different classes function in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "Moto.hpp"
+#include "Soigneur.hpp"
+#include "Soldat.hpp"
 #include "Vehicule.hpp"
 #include "Voiture.hpp"
+#include <iostream>
 
 int main() {
   Moto moto = Moto("BMW");
@@ -12,6 +15,14 @@ int main() {
   voiture.presenteToi();
   moto2->presenteToi();
   voiture2->presenteToi();
+
+  Soldat g1;                          // 30 points de vie
+  Soigneur s1;                        // 30 points de vie
+  std::cout << g1.vie() << std::endl; // renvoie 30
+  g1.attaque(&s1);                    // enlève 6 points de vie
+  std::cout << s1.vie() << std::endl; // renvoie 24
+  s1.soigne(&s1);                     // regagne 5 points de vie
+  std::cout << s1.vie() << std::endl; // renvoie 29
 
   return 0;
 }
